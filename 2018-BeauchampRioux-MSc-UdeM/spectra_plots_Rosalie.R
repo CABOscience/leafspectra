85ec2f0b934d6f9152da37f4f1f0df0cddd61bd6
Etienne Laliberté <etienne@MacBook-Pro-de-Etienne.local>
2018-11-05 14:10:51 -0500

    rosa

--- 2018-BeauchampRioux-MSc-UdeM/spectra_plots_Rosalie.R
+++ 2018-BeauchampRioux-MSc-UdeM/spectra_plots_Rosalie.R
@@ -1,1 +1,23 @@
 # visualisation spectra data Rosalie Beauchamp-Rioux
+
+library(tidyverse)
+
+# read all leaves from Rosalie project
+# average of six leaves (of leaf arrays)
+raw_leaves <- read_csv('data/project_leaves_combined.csv')
+
+# split into reflectance and transmittance
+refl <- raw_leaves %>% 
+  filter(reflectance_transmittance == 'reflectance')
+
+trans <- raw_leaves %>% 
+  filter(reflectance_transmittance == 'transmittance')
+     
+
+#                
+leafplot <- ggplot(refl, aes(x = wavelength, y = calculated_value)) +
+  geom_line() +
+  geom_line(aes(y = 1 - calculated_value),
+            data = leaves_trans) +
+  facet_grid(sample_id ~ leaf_number, ncol = 6) +
+  ylim(c(0,1))
